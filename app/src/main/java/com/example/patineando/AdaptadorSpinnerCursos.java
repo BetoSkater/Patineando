86ed14df1794e6f3080a10b8fe480ec65f7f1d74
BetoSkater <ajwolfcrow@gmail.com>
2021-08-19 18:43:34 +0200

    WIP funcionalidad de FragmentAnadirEditarCurso

---
+++ app/src/main/java/com/example/patineando/AdaptadorSpinnerCursos.java
@@ -0,0 +1,14 @@
+package com.example.patineando;
+
+import android.app.Activity;
+import android.widget.ArrayAdapter;
+/*
+public class AdaptadorSpinnerCursos extends ArrayAdapter<String> {
+    private Activity activity;
+    private ArrayList data;
+    public Resources res;
+    SpinnerModel
+}
+
+
+ */
--- app/src/main/java/com/example/patineando/FragmentsND/FragmentAnadirEditarCurso.java
+++ app/src/main/java/com/example/patineando/FragmentsND/FragmentAnadirEditarCurso.java
@@ -1,5 +1,7 @@
 package com.example.patineando.FragmentsND;
 
+import android.app.Dialog;
+import android.app.TimePickerDialog;
 import android.os.Bundle;
 
 import android.app.Fragment;
@@ -7,8 +9,15 @@ import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.TimePicker;
 
 import com.example.patineando.R;
+
+import java.util.Calendar;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -16,6 +25,17 @@ import com.example.patineando.R;
  * create an instance of this fragment.
  */
 public class FragmentAnadirEditarCurso extends Fragment {
+
+    private TextView txtMostrarHora;
+    private Button btnSeleccionarHora, btnConfirmar;
+
+    private int horaSeleccionador, minutoSeleccionador;
+
+    static final int TIME_DIALOG_ID = 0;
+
+    private Spinner spinnerCursos, spinnerLocalizacion, spinnerProfesores;
+
+
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -61,6 +81,85 @@ public class FragmentAnadirEditarCurso extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_anadir_editar_curso, container, false);
-    }
+        View vista =  inflater.inflate(R.layout.fragment_anadir_editar_curso, container, false);
+
+        txtMostrarHora = (TextView) vista.findViewById(R.id.txtHoraSeleccionada);
+        btnSeleccionarHora = (Button) vista.findViewById(R.id.btnSeleccionarHora);
+
+        spinnerCursos = (Spinner) vista.findViewById(R.id.sprSeleccionarTipoCurso);
+        spinnerLocalizacion = (Spinner) vista.findViewById(R.id.sprLocalizacionClases);
+        spinnerProfesores = (Spinner) vista.findViewById(R.id.sprSeleccionarProfesor);
+
+        //TODO si da tiempo, el Spinner de lso cursos debería tener una imagen, ver https://androidexample.com/Custom_Spinner_With_Image_And_Text_-_Android_Example/index.php?view=article_discription&aid=84&aaid=107
+
+        //Adaptador tipo cursos:
+        String [] cursos = {"Agresivo", "Artistico", "Baile", "Tecnica", "Hockey", "Slalom", "Velocidad", "Otros"};
+        ArrayAdapter<String> adaptadorCursos = new ArrayAdapter<String>(vista.getContext(), android.R.layout.simple_spinner_dropdown_item, cursos);
+        spinnerCursos.setAdapter(adaptadorCursos);
+
+
+        //Se añade un OnClickListener al botón en el que se llama a un método que muestra el dialog con el seleccionador de hora:
+        btnSeleccionarHora.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               /*
+                showDialog(TIME_DIALOG_ID);
+                //guia: https://www.bogotobogo.com/Android/android8DatePickerTimePickerClocks.php#TimePicker
+                //TODO a aprtir de aqui lo ponen fuera:
+                //Ahora se asigna a las variables horaSeleccionada y minutoSeleccionado los valores recogidos mediante la creacion de un objeto Calendar.
+
+                final Calendar calendario = Calendar.getInstance();
+                horaSeleccionador = calendario.get(Calendar.HOUR_OF_DAY);
+                minutoSeleccionador = calendario.get(Calendar.MINUTE);
+
+                //Mostrar fecha
+
+                actualizarHora();
+
+
+                */
+            }
+        });
+
+/*
+        protected Dialog onCreateDialog(int id){
+            switch (id){
+                case TIME_DIALOG_ID:
+                    return new TimePickerDialog(getContext(), horaSetListener, horaSeleccionador, minutoSeleccionador , false);
+            }
+            return null;
+        }//Fin override Dialog onCreateDialog
+
+        //Metodo que actualiza la hora en la caja de texto:
+        private void actualizarHora(){
+            txtMostrarHora.setText(new StringBuilder().append(pad(horaSeleccionador)).append(":").append(pad(minutoSeleccionador)));
+        }//Fin metodo actualizarHora().
+
+        //Respuesta obtenida cuando el usuario ha seleccionado una hora:
+         TimePickerDialog.OnTimeSetListener horaSetListener = new TimePickerDialog.OnTimeSetListener() {
+            @Override
+            public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                horaSeleccionador = hourOfDay;
+                minutoSeleccionador = minute;
+                actualizarHora();
+            }
+        };
+
+        //Metodo para  formatear la hora:
+        private static String pad(int c){
+            if (c >= 10){
+                return String.valueOf(c);
+            }else{
+                return "0" + String.valueOf(c);
+            }
+        }
+
+
+ */
+
+
+
+        return vista;
+
+    }//Fin onCreateView
 }
